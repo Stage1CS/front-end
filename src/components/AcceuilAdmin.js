f88eca64c8cd860cd13f5b8cf69c0125f3939067
Ikram Abidat <40496953+ikrami11@users.noreply.github.com>
2020-11-25 11:30:42 +0100

    admin

---
+++ src/components/AcceuilAdmin.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './AcceuilAdmin.css';
+import './CardsAdmin.css'
+import CardItemAdmin from './CardItemAdmin';
+
+function AcceuilAdmin() {
+  return (
+    <div className='hero-container'>
+      <video src='/videos/Office.mp4' autoPlay loop muted />
+      <h3>Qu'es ce que vous voulez faire ?</h3>
+
+      <div className='cards__container__admin'>
+        <div className='cards__wrapper__admin'>
+          <ul className='cards__items'>
+
+          <CardItemAdmin
+              src='images/satisfaction.jpg'
+              text='Rechercher'
+              path='/Recherche'
+            />
+             <CardItemAdmin
+              src='images/satisfaction.jpg'
+              text='Ajouter un livreur'
+              path='/Ajout_Livreur'
+            />
+            <CardItemAdmin
+              src='images/satisfaction.jpg'
+              text='Ajouter un point de vente'
+              path='/Ajout_Magasin'
+            /> 
+             <CardItemAdmin
+              src='images/satisfaction.jpg'
+              text='Importer mes données'
+              path='/Import'
+            />   
+             <CardItemAdmin
+              src='images/satisfaction.jpg'
+              text='Exporter me données'
+              path='/Export'
+            />               
+          </ul>
+        </div>
+      </div>
+          
+    </div>
+  );
+}
+
+export default AcceuilAdmin;
---
+++ src/components/pages/Admin.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+import AcceuilAdmin from '../AcceuilAdmin';
+
+
+function Admin() {
+    return (
+        
+                
+        <>
+           <AcceuilAdmin />
+        </>    
+           
+      );
+}
+
+export default Admin;
