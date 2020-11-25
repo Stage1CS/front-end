9534fca71c60069f45f3764d3ae56488ebdb8aa2
Ikram Abidat <40496953+ikrami11@users.noreply.github.com>
2020-11-25 12:11:35 +0100

    navigation bars

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import react from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
 import Connexion from './components/pages/Connexion';
@@ -11,7 +10,6 @@ function App() {
   return (
     <>
      <Router>
-       <Navbar/>
           <Route path='/' exact component={Home} />
           <Route path='/Connexion' exact component={Connexion} />
           <Route path='/Admin' exact component={Admin} />
