f24986b0de19afec8edfc60b52843a3698857290
Ikram Abidat <40496953+ikrami11@users.noreply.github.com>
2020-11-25 09:58:22 +0100

    changes form

--- src/components/pages/Connexion.js
+++ src/components/pages/Connexion.js
@@ -1,135 +1,47 @@
-import React, { Component } from "react";
-import "../../App.css";
+import React, {useState} from 'react';
+import '../../App.css';
+import Formulaire from '../Formulaire';
 
-const emailRegex = RegExp(
-  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-);
-
-const formValid = ({ formErrors, ...rest }) => {
-  let valid = true;
-
-  // validate form errors being empty
-  Object.values(formErrors).forEach(val => {
-    val.length > 0 && (valid = false);
-  });
-
-  // validate the form was filled out
-  Object.values(rest).forEach(val => {
-    val === null && (valid = false);
-  });
-
-  return valid;
-};
-
-class Connexion extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      firstName: null,
-      lastName: null,
-      email: null,
-      password: null,
-      formErrors: {
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: ""
-      }
-    };
-  }
-
-  handleSubmit = e => {
-    e.preventDefault();
-
-    if (formValid(this.state)) {
-      console.log(`
-        --SUBMITTING--
-        First Name: ${this.state.firstName}
-        Last Name: ${this.state.lastName}
-        Email: ${this.state.email}
-        Password: ${this.state.password}
-      `);
-    } else {
-      console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
-    }
-  };
-
-  handleChange = e => {
-    e.preventDefault();
-    const { name, value } = e.target;
-    let formErrors = { ...this.state.formErrors };
-
-    switch (name) {
-      case "firstName":
-        formErrors.firstName =
-          value.length < 3 ? "minimum 3 characaters required" : "";
-        break;
-      case "lastName":
-        formErrors.lastName =
-          value.length < 3 ? "minimum 3 characaters required" : "";
-        break;
-      case "email":
-        formErrors.email = emailRegex.test(value)
-          ? ""
-          : "invalid email address";
-        break;
-      case "password":
-        formErrors.password =
-          value.length < 6 ? "minimum 6 characaters required" : "";
-        break;
-      default:
-        break;
+function Connexion() {
+    const adminUser = {
+        email : "admin@admin.com",
+        password: "admin123"
     }
 
-    this.setState({ formErrors, [name]: value }, () => console.log(this.state));
-  };
+    const [user, setUser] = useState({name:"", email:""});
+    const [error, setError] = useState("");
 
-  render() {
-    const { formErrors } = this.state;
+    const Login = details => {
+        console.log(details);
+        if (details.email == adminUser.email && details.password == adminUser.password) {
+            console.log("Vous etes connecté");
+            setUser({
+                name: details.name,
+                email: details.email
+            });
+        } else {
+            console.log("Erreur");
+            setError("Erreur");
+        }
+    }
 
-    return (
-      <div className="wrapper">
-        <div className="form-wrapper">
-          <h1>Connexion</h1>
-          <form onSubmit={this.handleSubmit} noValidate>
-                    
-            <div className="email">
-              <label htmlFor="email">Email</label>
-              <input
-                className={formErrors.email.length > 0 ? "error" : null}
-                placeholder="Email"
-                type="email"
-                name="email"
-                noValidate
-                onChange={this.handleChange}
-              />
-              {formErrors.email.length > 0 && (
-                <span className="errorMessage">{formErrors.email}</span>
-              )}
-            </div>
-            <div className="password">
-              <label htmlFor="password">Mot de passe</label>
-              <input
-                className={formErrors.password.length > 0 ? "error" : null}
-                placeholder="mot de passe"
-                type="password"
-                name="password"
-                noValidate
-                onChange={this.handleChange}
-              />
-              {formErrors.password.length > 0 && (
-                <span className="errorMessage">{formErrors.password}</span>
-              )}
-            </div>
-            <div className="createAccount">
-              <button type="submit" >Se connecter</button>             
-            </div>
-          </form>
-        </div>
-      </div>
-    );
-  }
+    const Logout = () => {
+        setUser({name:"", email:""});
+    }
+
+  return (
+    <div className ="Connexion">
+        {(user.email != "") ? (
+             <div className ="Welcome">
+                  <h2>Welcome,  <span>{user.name}</span></h2>  
+                  <button onClick={Logout}>Logout</button>
+             </div>
+        ) : (
+            <Formulaire Login={Login} error={error}/>
+        )}
+
+    </div> 
+  );
 }
 
 export default Connexion;
