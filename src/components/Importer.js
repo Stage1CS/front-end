946b9a41cf3b708692276f98fcf2b20817088ecf
Ikram Abidat <40496953+ikrami11@users.noreply.github.com>
2020-11-26 18:17:32 +0100

    import export

--- src/components/Importer.js
+++ src/components/Importer.js
@@ -2,12 +2,31 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './Importer.css';
+import {Link} from 'react-router-dom';
+
 
 function Importer() {
   return (
 
-    <div className='hero-container'>
-      <img src='/images/rech.jpg' />
+    <div className='hero-container'> 
+    <video src='/videos/Office.mp4' autoPlay loop muted />    
+     <form>
+      <div className='form-inner'>
+
+      <h2>Veuillez importer les données vers quelle table ?</h2> 
+
+      <select name='' className='form-group'> 
+          <option value='Séléctionner'>Séléctionner</option>
+          <option value='Livreur'>Livreur</option>
+          <option value='Magasin'>Magasin</option>      
+      </select>
+
+      <Link to='/Admin' className='nav-links'>
+            <input type="submit" value="Importer"/> 
+      </Link>      
+
+     </div>
+    </form>
     </div>
 
   );
--- src/components/Rech.js
+++ src/components/Rech.js
@@ -2,12 +2,45 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './Rech.css';
+import {Link} from 'react-router-dom';
+
 
 function Rech() {
   return (
 
-    <div className='hero-container'>
-      <img src='/images/rech.jpg' />
+    <div className='hero-container__livreur'> 
+    <video src='/videos/Office.mp4' autoPlay loop muted />    
+     <form>
+      <div className='form-inner'>
+
+      <h2>Veuillez remplir les informations de votre livreur</h2> 
+
+      <div className='form-group'> 
+          <label htmlFor="name">Nom :</label>
+          <input type="text" name="name" id="name" />     
+      </div>
+
+      <div className='form-group'> 
+          <label htmlFor="">Prénom :</label>
+          <input type="text" name="=" id="" />     
+      </div>
+
+      <div className='form-group'> 
+          <label htmlFor="email">Email :</label>
+          <input type="email" name="email" id="email" />     
+      </div>
+
+      <div className='form-group'> 
+          <label htmlFor="">Numéro de téléphone :</label>
+          <input type="text" name="" id="" />     
+      </div>
+
+      <Link to='/Admin' className='nav-links'>
+            <input type="submit" value="Envoyer"/> 
+      </Link>      
+
+     </div>
+</form>
     </div>
 
   );
