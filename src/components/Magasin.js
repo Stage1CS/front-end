ee4f713096b3ca45b02a2c2825b8a2dc1fa6c103
Ikram Abidat <40496953+ikrami11@users.noreply.github.com>
2020-11-29 15:02:40 +0100

    markeur on map

--- src/components/Magasin.js
+++ src/components/Magasin.js
@@ -1,88 +1,94 @@
 import React from 'react';
-import '../App.css';
-import { Button } from './Button';
-import './Rech.css';
-import './Magasin.css';
-import { MapContainer, Marker, TileLayer } from 'react-leaflet';
-import {Link} from 'react-router-dom';
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import {Link} from 'react-router-dom';
 
+class Magasin extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            markers: [[35.6892, 51.3890]],
+        };
+    }
 
-function GetIcon(_iconSize, forecast) {
-    return L.icon({
-      iconUrl: require("../Static/Icons/map-marker.png"),
-      iconSize: [_iconSize],
-    });
-  }
+    addMarker = (e) => {
+        const { markers } = this.state;
+        markers.pop();
+        markers.push(e.latlng);
+        this.setState({ markers });
+    }
 
-function Magasin() {
- 
-  return (
+    render() {
+        let DefaultIcon = L.icon({
+            iconUrl: icon,
+            shadowUrl: iconShadow
+        });
+        L.Marker.prototype.options.icon = DefaultIcon;
 
-    <div className='hero-container'>
-      <video src='/videos/Office.mp4' autoPlay loop muted />    
-     <form>
-      <div className='form-inner'>
+        return (
+            <div className='hero-container'>
+                <form>
+                    <div className='form-inner'> 
 
-      <h2>Veuillez remplir les informations sur le point à ajouter </h2> 
+                        <h2>Veuillez remplir les informations sur le point à ajouter </h2> 
 
-      <div className='form-group'> 
-          <label htmlFor="nom magasin">Nom :</label>
-          <input type="text" name="nom magasin" id="nom magasin" />     
-      </div>
+                        <div className='form-group'> 
+                             <label htmlFor="nom magasin">Nom :</label>
+                             <input type="text" name="nom magasin" id="nom magasin" />     
+                        </div>
 
-      <div className='form-group'> 
-          <label htmlFor="email_proprietaire">Email :</label>
-          <input type="email" name="email_proprietaire" id="email_proprietaire" />     
-      </div>
+                         <div className='form-group'> 
+                              <label htmlFor="email_proprietaire">Email :</label>
+                             <input type="email" name="email_proprietaire"  id="email_proprietaire" />     
+                         </div>
 
-      <div className='form-group'> 
-          <label htmlFor="">Numéro de téléphone :</label>
-          <input type="text" name="" id="" />     
-      </div> 
+                         <div className='form-group'> 
+                              <label htmlFor="">Numéro de téléphone :</label>
+                              <input type="text" name="" id="" />     
+                         </div> 
 
-      <div className='leaflet-container'> 
+            <div className='leaflet-container'>            
+                <MapContainer
+                    center={[35.6892, 51.3890]}
+                    onClick={this.addMarker}
+                    zoom={13}
+                    maxZoom={18}
+                    minZoom={5}   
+                    style={{width: '100%',height: '400px'}}
+                >
+                    <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                    />
+                    {this.state.markers.map((position, idx) =>
+                        <Marker key={`marker-${idx}`} position={position}></Marker>
+                    )}
+                </MapContainer>
+            </div>
 
-            <MapContainer className="markercluster-map" center={[51.0, 19.0]} zoom={4} maxZoom={18}>
+                        <div className='form-group'> 
+                              <label htmlFor=""> longitude :</label>
+                              <input type="text" name="" id="" />     
+                        </div>
 
-                <TileLayer
-                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
- 
-                />
+                        <div className='form-group'> 
+                              <label htmlFor="">latitude :</label>
+                              <input type="text" name="" id="" />     
+                        </div>
 
-                <Marker position={[36.721997, 3.1901515]}>
+                        <Link to='/Admin' className='nav-links'>
+                             <input type="submit" value="Envoyer"/> 
+                         </Link>      
 
-
-                </Marker>
-
-            </MapContainer>
-
-      </div>
+                    </div>
     
-      <div className='form-group'> 
-          <label htmlFor=""> longitude :</label>
-          <input type="text" name="" id="" />     
-      </div>
-      <div className='form-group'> 
-          <label htmlFor="">latitude :</label>
-          <input type="text" name="" id="" />     
-      </div>
-
-      <Link to='/Admin' className='nav-links'>
-            <input type="submit" value="Envoyer"/> 
-      </Link>      
-
-     </div>
-    
-</form>
-    </div>
-
-  );
+                </form>
+            </div>
+        );
+    }
 }
 
 export default Magasin;
