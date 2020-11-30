ab8cb0370022d526238c52dc510435801c12b60d
Ikram Abidat <40496953+ikrami11@users.noreply.github.com>
2020-11-30 16:15:47 +0100

    rech liv mag

---
+++ src/components/pages/Recherche_liv.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+import Rech_liv from '../Rech_liv';
+import NavbarAdmin from '../NavbarAdmin';
+
+function Recherche_liv() {
+    return (
+                       
+        <>
+           <NavbarAdmin/>
+           <Rech_liv />
+        </>    
+           
+      );
+}
+
+export default Recherche_liv;
