3379cf5e5c01e33a42711f600d1df845178537a0
Ikram Abidat <40496953+ikrami11@users.noreply.github.com>
2020-11-30 12:43:31 +0100

    zbel

--- src/components/Magasin.js
+++ src/components/Magasin.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import {  TileLayer, Marker, Popup } from 'react-leaflet';
+import {TileLayer, Marker, Popup } from 'react-leaflet';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import {Link} from 'react-router-dom';
