0d81b3c644204fa4200afd1704b615fb46b9ac9a
Save Point <mehdidouy@gmail.com>
2020-12-03 15:40:27 +0100

    Update Rech_mag.js

--- src/components/Rech_mag.js
+++ src/components/Rech_mag.js
@@ -13,7 +13,7 @@ function Rech_mag() {
   const location = useLocation();
   var token = location.state.token;
   console.log("token ->"+token)
-  const [data, setData] = useState('')
+  const [data, setData] = useState([])
   const [email, setEmail] = useState('')
   const [phone, setPhone] = useState('')
   const [rech, setRech] = useState('')
@@ -41,10 +41,10 @@ useEffect(() => {
      <input type="text" name="name" id="nom" type="text" placeholder="Recherche..." onChange={Event => {setRech(Event.target.value)} }/>     
  <h2>
 
- {JSONDATA
+ {data
  .filter(val => {return  (val.first_name.toLocaleLowerCase().includes(rech.toLocaleLowerCase())) || (val.email.toLocaleLowerCase().includes(rech.toLocaleLowerCase())) || (val.last_name.toLocaleLowerCase().includes(rech.toLocaleLowerCase()))})
  .map((val,key) => {
-   return<div className='form-inner'><p> {val.last_name} {val.email} {val.number}</p></div>
+ return<div className='form-inner'><p> {val.Phone} {val.email} {val.name}{val.detail }</p></div>
  }
  )}
 
