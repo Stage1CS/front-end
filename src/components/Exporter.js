839237e48c0e71e8280b2b30676e67b7c721820d
Ikram Abidat <40496953+ikrami11@users.noreply.github.com>
2020-12-06 15:11:09 +0100

    latest version

--- src/components/Exporter.js
+++ src/components/Exporter.js
@@ -1,32 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../App.css';
 import './Exporter.css';
 import {Link} from 'react-router-dom';
 import {useLocation} from 'react-router-dom';
+import reactHTMLTableToExcel from 'react-html-table-to-excel';
+import axios from 'axios';
+
 
 function Exporter(props) {
     const location = useLocation();
     var token = location.state.token;
     console.log("token :  ->"+token)
+
+    const submitHandler = e => {
+      e.preventDefault();
+    }
+
+    const ExportDataM = () => {
+        fetch('https://laravelapi.ouedsmar.com/public/api/exportmagasin?token='+token, {
+        method:'GET',
+        headers: new Headers({
+          'Content-Type': 'application/x-www-form-urlencoded',
+          }),
+        });
+      }
+
+     const ExportDataL = () => {
+      fetch('https://laravelapi.ouedsmar.com/public/api/exportlivreur?token='+token, {
+        method:'GET',
+        headers: new Headers({
+          'Content-Type': 'application/x-www-form-urlencoded',
+          }),
+        });
+      }
+
     
   return (
 
     <div className='hero-container'> 
     <video src='/videos/Office.mp4' autoPlay loop muted />    
-     <form>
+
+     <form onSubmit={submitHandler}>
       <div className='form-inner'>
 
       <h2>Vous voullez exporter les données à partir de quelle table ?</h2> 
 
-      <select name=''> 
-          <option value='Séléctionner'>Séléctionner</option>
-          <option value='Livreur'>Livreur</option>
-          <option value='Magasin'>Magasin</option>      
-      </select>
+      <Link to='/Admin' className='nav-links'>
+            <input type="submit" onClick={ExportDataM()} value="Exporter Magasins"/> 
+      </Link>
 
       <Link to='/Admin' className='nav-links'>
-            <input type="submit" value="Exporter"/> 
-      </Link>      
+            <input type="submit" onClick={ () => ExportDataL()}  value="Exporter Livreurs"/> 
+      </Link>        
 
      </div>
     </form>
--- src/components/Magasin.js
+++ src/components/Magasin.js
@@ -1,58 +1,81 @@
 import React from 'react';
-import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import {Link} from 'react-router-dom';
 import './Magasin.css';
 import {useLocation} from 'react-router-dom';
-import {setPostName , useEffect} from 'react';
+import {useState} from 'react';
 
 function Magasin(props) {
 
     const location = useLocation();
     var token = location.state.token;
     console.log("magasin :  ->"+token);
+
+    const[name, setName] =useState("");
+    const[detail, setDetail] =useState("");
+    const[Phone, setPhone] =useState("");
+    const[email, setEmail] =useState("");
+    const[lat, setLat] =useState("");
+    const[lng, setLng] =useState("");
+
+    const submitHandler = e => {
+                    e.preventDefault();
+    }
+    
+    const postData = () => {
+        fetch(' https://laravelapi.ouedsmar.com/public/api/magasin?token='+token, {
+            method:'POST',
+            headers: new Headers({
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }),
+            body: "name="+name+"&detail="+detail+"&Phone="+Phone+"&email="+email+"&lat="+lat+"&lng="+lng
+        }); 
+    }
     
         return (
             <div className='hero-container'>
                 <video src='/videos/Office.mp4' autoPlay loop muted />
-                <form>
+
+                <form onSubmit={submitHandler}>
                 <div className='haha'>
                     <div className='form-inner'> 
 
                         <h2>Veuillez remplir les informations sur le point à ajouter </h2>        
 
                         <div className='form-group'> 
-                             <label htmlFor="nom magasin">Nom :</label>
-                             <input type="text" name="nom magasin" id="nom magasin" />     
-                        </div>
-
-                         <div className='form-group'> 
-                              <label htmlFor="email_proprietaire">Email :</label>
-                             <input type="email" name="email_proprietaire"  id="email_proprietaire" />     
-                         </div>
-
-                         <div className='form-group'> 
-                              <label htmlFor="">Numéro de téléphone :</label>
-                              <input type="text" name="" id="" />     
-                         </div> 
-
-            <div id='map'>            
-                
-            </div> 
-                      <div className='form-group'> 
-                              <label htmlFor=""> longitude :</label>
-                              <input type="text" name="lng" id="lng" />     
+                             <label htmlFor="name">Nom :</label>
+                             <input type="text" name="name" id="name" onChange={e => setName()} value={name}/>     
                         </div>
 
                         <div className='form-group'> 
-                              <label htmlFor="">latitude :</label>
-                              <input type="text" name="lat" id="lat" />     
+                              <label htmlFor="detail">Description detaillée du magasin :</label>
+                             <input type="text" name="detail"  id="detail" onChange={e => setDetail()} value={detail}/>     
+                        </div>
+
+                        <div className='form-group'> 
+                              <label htmlFor="Phone">Numéro de téléphone :</label>
+                              <input type="text" name="Phone" id="Phone" onChange={e => setPhone()} value={Phone}/>     
+                        </div> 
+
+                        <div className='form-group'> 
+                              <label htmlFor="email">Email :</label>
+                              <input type="text" name="email" id="email" onChange={e => setEmail()} value={email}/>     
+                        </div>
+
+                        <div id='map'></div> 
+
+                        <div className='form-group'> 
+                              <label htmlFor="lng"> longitude :</label>
+                              <input type="text" name="lng" id="lng" onChange={e => setLng()} value={lng}/>     
+                        </div>
+
+                        <div className='form-group'> 
+                              <label htmlFor="lat">latitude :</label>
+                              <input type="text" name="lat" id="lat" onChange={e => setLat()} value={lat}/>     
                         </div>
 
                         <Link to='/Admin' className='nav-links'>
-                             <input type="submit" value="Envoyer"/> 
+                             <input type="submit" onClick={ () => postData()} value="Envoyer"/> 
                          </Link>      
 
                     </div>
