f55e21994af93084c1daa6dad494e50404a04a7b
Ikram Abidat <40496953+ikrami11@users.noreply.github.com>
2020-12-12 18:35:38 +0100

    push

--- src/components/Importer.js
+++ src/components/Importer.js
@@ -43,30 +43,30 @@ function Importer(props) {
     };   
 }
 
-/*function  read_m(v) {
-      let files = v.target.files;
-      let reader = new FileReader();
-      const csv = files[0]
-      reader.readAsText(csv)
+/*function  read_m(e) {
+  let files = e.target.files;
+  let reader = new FileReader();
+  const csv = files[0]
+  reader.readAsText(csv)
 
-      reader.onload = m => {
+  reader.onload = r => {
 
-      var data_m = m.target.result
-      var splitted_data_m = data_m.split('\n');
-      splitted_data_m.shift();
-      console.log(splitted_data_m[0].split(','));
-      console.log(splitted_data_m)
+      var data = r.target.result
+      var splitted_data = data.split('\n');
+      splitted_data.shift();
+      console.log(splitted_data[0].split(','));
+      console.log(splitted_data)
       
-      for(var i =0;i<splitted_data_m.length;i++){
-        var data_m2 = splitted_data_m[i].split(',')
-        var name = data_m2[1];
-        var detail = data_m2[2];
-        var Phone= data_m2[3];
-        var email = data_m2[4];
-        var lat = data_m2[5];
-        var lng = data_m2[6];
-        var updated_at = data_m2[7];
-        var created_at = data_m2[8];
+      for(var i =0;i<splitted_data.length;i++){
+        var data2 = splitted_data[i].split(',')
+        var name = data2[1];
+        var detail = data2[2];
+        var Phone= data2[3];
+        var email = data2[4];
+        var lat = data2[5];
+        var lng = data2[6];
+        var updated_at = data2[7];
+        var created_at = data2[8];
 
         console.log(name,detail,Phone,email,lat,lng,updated_at,created_at,props.token)
         
@@ -76,10 +76,11 @@ function Importer(props) {
           headers: new Headers({
               'Content-Type': 'application/x-www-form-urlencoded',
           }),
-           body: "name="+name+"&detail="+detail+"&Phone="+Phone+"&email="+email+"&lat="+lat+"&lng="+lng+"&updated_at="+updated_at+"&created_at="+created_at
+          body: "name="+name+"&detail="+detail+"&Phone="+Phone+"&email="+email+"&lat="+lat+"&lng="+lng+"&updated_at="+updated_at+"&created_at="+created_at
       })
-    }   
-  };
+    }
+    
+  };   
 }*/
 
   return (
@@ -95,7 +96,7 @@ function Importer(props) {
       <input type="file" name="file" onChange={e => read(e)} />
 
       <h2>Magasins :</h2>
-      <input type="file" name="file" /*onChange={v => read_m(v)} *//>
+      <input type="file" name="file" /*onChange={e => read_m(e)}*//>
 
      </div>
     </form>
